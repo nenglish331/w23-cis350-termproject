aad2f0604d49e903cfbc2627fd63c874237ac5e8
Nathan English <nenglish331@gmail.com>
2023-02-07 19:03:41 -0500

    Added GUI and Test files

---
+++ src/w23_cis350_termproject/BlackjackTest.java
@@ -0,0 +1,60 @@
+package w23_cis350_termproject;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BlackjackTest {
+   @Test
+   public void testDeckInitialization() {
+      // Test initialization of deck of cards
+   }
+
+   @Test
+   public void testShuffleDeck() {
+      // Test shuffling of deck of cards
+   }
+
+   @Test
+   public void testDrawCard() {
+      // Test drawing a card from the deck
+   }
+
+   @Test
+   public void testCardValue() {
+      // Test getting value of a card
+   }
+
+   @Test
+   public void testHandInitialization() {
+      // Test initialization of hand
+   }
+
+   @Test
+   public void testAddCardToHand() {
+      // Test adding a card to hand
+   }
+
+   @Test
+   public void testGetHandScore() {
+      // Test getting score of hand
+   }
+
+   @Test
+   public void testHandBusted() {
+      // Test checking if hand is busted
+   }
+
+   @Test
+   public void testPlayerHit() {
+      // Test player hitting
+   }
+
+   @Test
+   public void testDealerHit() {
+      // Test dealer hitting
+   }
+
+   @Test
+   public void testDealerShouldHit() {
+      // Test dealer deciding whether to hit
+   }
+}
