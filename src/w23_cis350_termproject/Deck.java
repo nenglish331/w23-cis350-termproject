4ca899339061186b194fbb2f16cd1c6432266c7d
Nathan English <nenglish331@gmail.com>
2023-02-12 13:17:13 -0500

    Update Deck.java

--- src/w23_cis350_termproject/Deck.java
+++ src/w23_cis350_termproject/Deck.java
@@ -10,16 +10,16 @@ public class Deck {
 		String[] suits = {"♠", "♥", "♦", "♣"};
 		String[] ranks = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
 		deck = new ArrayList<Card>();
-	    for (String suit : suits) {
-	    	for (String rank : ranks) {
-	    		deck.add(new Card(rank, suit));
+	    	for (String suit : suits) {
+	    		for (String rank : ranks) {
+	    			deck.add(new Card(rank, suit));
+	    		}
 	    	}
-	    }
 	}
 
 	public void shuffle() {
 		// Shuffle deck of cards
-	    Collections.shuffle(deck);
+	    	Collections.shuffle(deck);
 	}
 
 	public Card draw() {
