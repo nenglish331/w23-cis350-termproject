847a0b4e47d77f61ffb305de072dea4bc40fc8d9
sarahwight14 <78120880+sarahwight14@users.noreply.github.com>
2023-03-24 09:48:01 -0400

    added javadoc comments

--- src/game/Dealer.java
+++ src/game/Dealer.java
@@ -13,21 +13,45 @@ public class Dealer {
     this.hand = new Hand();
   }
 
+  /**
+   * Player requests an extra card from the deck. A card is drawn
+   * and added to the current hand
+   *
+   * @param deck  the current deck of cards
+   */
   public void hit(Deck deck) {
     // Draw a card from the deck
     hand.addCard(deck.draw());
   }
 
+  /**
+   * Decides if the dealer should hit (draw a card from the deck)
+   * based on the hand score
+   *
+   * @return  the decision of whether the dealer should hit
+   */
   public boolean shouldHit() {
     // Decide if dealer should hit based on hand score
     return hand.getScore() < 17;
   }
 
+  /**
+   * The player's hand contains two cards.
+   *
+   * @return  the player's hand
+   */
   public Hand getHand() {
     // Get player's hand
     return hand;
   }
-
+  
+  /**
+   * The player's score is determined by the point total of the cards.
+   * Cards 2 through 10 are worth their face value, and face cards
+   * (jack, queen, king) are also worth 10.
+   *
+   * @return  the score based on the point total of the cards in the hand
+   */
   public int getScore() {
     // Get player's total score
     return hand.getScore();
